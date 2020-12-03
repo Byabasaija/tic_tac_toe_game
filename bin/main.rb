619cb80177ff8c9780c52ba9f7774ce2390440d5
Your Name <basaijapascal9@gmail.com>
2020-12-03 22:47:12 +0300

    display the board

--- bin/main.rb
+++ bin/main.rb
@@ -10,3 +10,21 @@ player1 = gets.chomp
 
 puts "Enter player2 name"
 player2 = gets.chomp
+
+puts "TIC TAC TOE"
+
+puts "play by entering X or O on the board"
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
+ display_board(board)
+
+ puts "You won"
+ puts "You Drew"
