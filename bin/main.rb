9e22867374ff4753c8fbb94dd1eae85b54d8afe6
Hillary Okello <hillariouskelly@outlook.com>
2020-12-03 23:00:38 +0300

    fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,30 +1,21 @@
-# welcome message for the users
-# ask for user details
-# information about the game
-# instructions on how to play
-
 puts "Welcome to Pascal and Hillary's tic-tac-toe game"
 
-puts "Enter player1 name"
-player1 = gets.chomp
+puts 'Enter name'
 
-puts "Enter player2 name"
-player2 = gets.chomp
+puts 'TIC TAC TOE'
 
-puts "TIC TAC TOE"
+puts 'play by entering X or O on the board'
 
-puts "play by entering X or O on the board"
-
-board = [" "," "," "," "," "," "," "," "," "]
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
- end
- display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board(board)
 
- puts "You won"
- puts "You Drew"
+puts 'You won'
+puts 'You Drew'
