062dceeeecbbfb9cb8a501029b8b9594d3b72fe7
Hillary Okello <hillariouskelly@outlook.com>
2020-12-03 20:55:58 +0300

    prompt users for their names

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,12 @@
-puts 'Hello World!'
+# welcome message for the users
+# ask for user details
+# information about the game
+# instructions on how to play
+
+puts "Welcome to Pascal and Hillary's tic-tac-toe game"
+
+puts "Enter player1 name"
+player1 = gets.chomp
+
+puts "Enter player2 name"
+player2 = gets.chomp
