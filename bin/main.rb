057637b45679d5199bbfeac691730c79ae3b6f2d
Hillary Okello <hillariouskelly@outlook.com>
2020-12-10 14:06:33 +0300

    fix conflicts

--- bin/main.rb
+++ bin/main.rb
@@ -1,107 +1,66 @@
 #!/usr/bin/env ruby
+puts "Welcome to Pascal and Hillary's tic-tac-toe game"
 
-require_relative '../lib/game_logic.rb'
-require_relative '../lib/play.rb'
+puts 'Enter first player name'
+player_names = Array.new(2)
+invalid = true
+while invalid
+  (0..1).each do |i|
+    puts "Enter player #{i + 1} name"
+    player_names[i] = gets.chomp
+  end
+  invalid = false
+end
+puts 'TIC TAC TOE'
 
-class UserInterface
-  def initialize
-    @game_logic = GameLogic.new
+puts 'play by entering X or O on the board'
+
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board(board)
+
+game_on = true
+
+puts 'Please enter your token (X or O)'
+token = gets.chomp!
+
+while game_on
+
+  board << token.to_i
+  if token == 'X'
+    puts 'The other players turn '
+  else
+    puts 'Your turn'
   end
 
-  def welcome
-    puts 'TIC TAC TOE'
-    puts "Welcome to Pascal's and Hillary's tic-tac-toe game"
-    player = names_of_players
-    player_symbols(player)
-    show_board
-    game
-    display_board(@board)
+  puts 'Place your token (X or O) on positions 1-9 on the board'
+
+  puts <<-BOARD
+    |X|O|X|
+    |X|O|X|
+    |X|O|X|
+  BOARD
+
+  if board << token.to_i == 'X' || board << token.to_i == 'O'
+    puts 'Invalid move'
+  else
+    # play on
+    puts 'Valid move'
   end
 
-  def names_of_players
-    player_names = Array.new(2)
-    invalid = true
-    while invalid
-      (0..1).each do |i|
-        puts "Enter player #{i + 1} name"
-        player_names[i] = gets.chomp
-      end
-
-      if player_names[0] == player_names[1]
-        puts "Player names can't be the same"
-        invalid = true
-
-      elsif player_names[0].empty? || player_names[1].empty?
-        puts "player names can't be empty"
-        invalid = true
-      else
-        invalid = false
-      end
-    end
-    player_names
-  end
-
-  def player_symbols(player)
-    puts ("#{player[0]} symbol is X and #{player[1]} symbol is O")
-  end
-
-  def show_board
-    puts 'play by entering X or O on the board by selecting 1-9'
-    puts <<-BOARD
-      1 | 2 | 3 |
-      -----------
-      4 | 5 | 6 |
-      -----------
-      7 | 8 | 9 |
-    BOARD
-  end
-
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts '-----------'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  end
-
-  def game
-    game_on = true
-    i = 0
-    exp = '123456789'.split('')
-    while game_on
-
-      input = gets.chomp
-      # index = @game_logic.input_to_index(input)
-
-      if exp.include?(input)
-        puts @game_logic.current_player(i)
-        # @board [@game_logic.input_to_index(input)] = @game_logic.current_player(i)
-        
-        display_board(@game_logic.board)
-        i += 1
-      else
-        puts "Enter Valid Input"
-      end
-
-      if @game_logic.valid_move?(input)
-        @game_logic.move(input, i)
-      else
-        puts "position taken"
-      end
-
-      # valid = @game_logic.position_taken?(input)
-
-      # if valid = @game_logic.current_player(i)
-      #   @board[input.to_i-1] = @game_logic.current_player(i)
-      # else
-      #   puts 'position taken'
-      # end
-
-      # @game_logic.valid_move?(input.to_i)
-      # return 'Position taken' unless @game_logic.position_taken?(input)
-
-    end
+  winner = true
+  # player tokens are alligned in the entire row or column
+  if winner
+    puts 'You won'
+    game_on = false
+  else
+    puts "It's a draw"
   end
 end
-user_interface = UserInterface.new
-user_interface.welcome
