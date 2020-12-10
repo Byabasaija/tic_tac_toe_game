d5c31e6c9c628a24cffa2572c30f478b82b06ad0
Hillary Okello <hillariouskelly@outlook.com>
2020-12-10 19:00:31 +0300

    fixed linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+# rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/BlockNesting
 require_relative '../lib/game_logic'
 require_relative '../lib/play'
 class UserInterface
@@ -20,7 +20,6 @@ class UserInterface
   def names_of_players
     player_names = Array.new(2)
     invalid = true
-    num = '0123456789'.split('')
     while invalid
       (0..1).each do |i|
         puts "Enter player #{i + 1} name"
@@ -30,8 +29,12 @@ class UserInterface
       if player_names[0] == player_names[1]
         puts "Player names can't be the same"
         invalid = true
-        
-      elsif ('0123456789').include?(player_names[0]) || ('0123456789').include?(player_names[1]) || player_names[0].include?(' ') || player_names[1].include?(' ')
+
+      elsif ('0123456789').include?(player_names[0]) || ('0123456789').include?(player_names[1])
+        puts 'Enter a valid name'
+        invalid = true
+
+      elsif player_names[0].include?(' ') || player_names[1].include?(' ')
         puts 'Enter a valid name'
         invalid = true
 
@@ -67,52 +70,53 @@ class UserInterface
     puts '-----------'
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
-  
+
   def game
     game_on = true
     i = 0
     exp = '123456789'.split('')
     while game_on
-      if @game_logic.turn == 0
-       puts ("#{@player[0]} starts the game")
-      end
+      puts("#{@player[0]} starts the game") if @game_logic.turn.zero?
       input = gets.chomp
       if @game_logic.valid_move?(input)
         @game_logic.move(input, i) && @game_logic.position_taken?(input)
         display_board(@game_logic.board)
-          if @game_logic.won(@game_logic.board) == 1 ||@game_logic.won(@game_logic.board) == 2
-            case @game_logic.won(@game_logic.board)
-            when 1
-              abort("#{@player[0]} wins!")
-            when 2
-              abort("#{@player[1]} wins!")
-            end
-          elsif
-            if @game_logic.turn.even?
-              puts("#{@player[0]}'s turn")
-            else
-            puts("#{@player[1]}'s turn")
-            end
+
+        if @game_logic.won(@game_logic.board) == 1 || @game_logic.won(@game_logic.board) == 2
+          case @game_logic.won(@game_logic.board)
+          when 1
+            abort("#{@player[0]} wins!")
+          when 2
+            abort("#{@player[1]} wins!")
           end
+        elsif @game_logic.turn.even?
+          puts("#{@player[0]}'s turn")
+        else
+          puts("#{@player[1]}'s turn")
+        end
         if @game_logic.turn == 9
           abort("It's a draw")
         else
           game_on
         end
         i += 1
+
       elsif exp.include?(input) && @game_logic.turn < 8
         @game_logic.position_taken?(input)
         display_board(@game_logic.board)
         puts 'Position Taken Try Again'
+
       elsif @game_logic.full?
         abort("It's a draw")
+
       else
         puts 'Enter Valid Input'
       end
+
     end
   end
 end
 user_interface = UserInterface.new
 user_interface.welcome
 
-# rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+# rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/BlockNesting
---
+++ j
@@ -0,0 +1,13 @@
+  develop[m
+* [32mgame-logic-feature[m
+  readme_game_instructions[m
+  set-up-feature[m
+  user-interface[m
+  user_interface[m
+  [31mremotes/origin/HEAD[m -> origin/develop
+  [31mremotes/origin/develop[m
+  [31mremotes/origin/game-logic-feature[m
+  [31mremotes/origin/main[m
+  [31mremotes/origin/readme_game_instructions[m
+  [31mremotes/origin/set-up-feature[m
+  [31mremotes/origin/user_interface[m
